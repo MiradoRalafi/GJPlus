7590e6e8bfdc176d47605df93da886e0edcceb79
MiradoRalafi <mralafiarindaza@gmail.com>
2020-10-10 19:29:00 +0300

    Parallax Serpent

---
+++ Assets/Scripts/Background/ParallaxController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    public float Speed;
+    public float Offset;
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * Speed * Time.deltaTime);
+        if(transform.localPosition.x < -Offset)
+        {
+            transform.localPosition = Vector2.right * Offset;
+        }
+    }
+}
