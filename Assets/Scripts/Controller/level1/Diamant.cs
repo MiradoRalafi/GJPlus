859a12713d6aed24173df94405b24991bb7aba9b
Chad <thongsangchad@gmail.com>
2020-10-10 21:07:56 +0100

    Level1 Integration des 2 parties

---
+++ Assets/Scripts/Controller/level1/Diamant.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diamant : MonoBehaviour
+{
+    public bool isClicked=false;
+    public float delay=1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        isClicked = true;
+        StartCoroutine(Wait());
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(delay);
+        gameObject.SetActive(false);
+    }
+}
