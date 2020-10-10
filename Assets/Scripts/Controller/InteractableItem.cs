5f72637769751b2612b0869d34770d9b3f9348af
Chad <thongsangchad@gmail.com>
2020-10-10 18:39:06 +0100

    aDD SCRITP

---
+++ Assets/Scripts/Controller/InteractableItem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableItem : MonoBehaviour
+{
+    public bool canOpen;
+    public GameObject toOpen;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        if (canOpen)
+        {
+            toOpen.SetActive(true);
+        }    
+    }
+    public void closePanel()
+    {
+        toOpen.SetActive(false);
+    }
+
+}
