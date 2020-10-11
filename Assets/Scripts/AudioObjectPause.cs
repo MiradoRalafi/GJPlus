10faac24cac688c0a3844b368f9eecf6d75e6123
Zulmianah <40556052+zulmianah@users.noreply.github.com>
2020-10-11 23:40:43 +0300

    stop

--- Assets/Scripts/AudioObjectPause.cs
+++ Assets/Scripts/AudioObjectPause.cs
@@ -7,7 +7,7 @@ public class AudioObjectPause : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        AudioManager.StoMusic();
     }
 
     // Update is called once per frame
