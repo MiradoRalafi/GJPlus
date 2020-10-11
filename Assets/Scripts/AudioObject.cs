83102f9a52dd992975d37998f5dd9271262da6c8
Zulmianah <40556052+zulmianah@users.noreply.github.com>
2020-10-11 23:12:51 +0300

    main menu

---
+++ Assets/Scripts/AudioObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioObject : MonoBehaviour
+{
+    public int selector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioManager.PlayMusicSelector(selector);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
