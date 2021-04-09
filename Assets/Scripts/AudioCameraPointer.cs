2852aff235c2115a680c174b9f75bdf30b973bab
Zulmianah <40556052+zulmianah@users.noreply.github.com>
2021-04-09 06:19:10 +0300

    sound

---
+++ Assets/Scripts/AudioCameraPointer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioCameraPointer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        makeCamFollowPointer( );
+    }
+
+    public void makeCamFollowPointer( ){
+        float speed = 20f;
+        transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed, 0f);
+    }
+}
---
+++ Assets/Scripts/AudioUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void isMouseOverUI(string path){
+        AudioManager.PlayOneShot(path);
+    }
+}
